773bb2aabe1cb58f8c44169e10816e12c0e5a0b3
Seyeon Choi <103797536+seyeon22@users.noreply.github.com>
2025-04-09 19:53:50 +0900

    [Gold V] Title: 적록색약, Time: 128 ms, Memory: 16564 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -59,11 +59,7 @@ public class Main {
             }
 
             for (int i = 0; i < n; i++) {
-                if (!visit[i][0]) {
-                    cnt++;
-                    bfs(i, 0);
-                }
-                for (int j = 1; j < n; j++) {
+                for (int j = 0; j < n; j++) {
                     if (!visit[i][j]) {
                         cnt++;
                         bfs(i, j);
